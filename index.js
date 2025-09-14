d8588d198eae08e5fcc2d7921a892e1cd93e5252
sabbir-063 <202114063@student.mist.ac.bd>
2025-09-14 19:13:34 +0600

    hostname changed

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.use('/api/contact', contactRoutes);
 
 
 PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
